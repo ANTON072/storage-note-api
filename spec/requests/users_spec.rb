5c42e9244b12bc2d0af243ea4d4db0387377b510
Katsushi OUGI <ougi@to-r.net>
2023-06-29 23:22:30 +0900

    Request Specの追加

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,45 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Api::V1::Users', type: :request do
+  let(:firebase_user) { { user_id: 1, email: 'hoge@hoge.com' } }
+  let(:user_params) { { user: { name: 'new_user', photo_url: 'https://example.com/photo.jpg' } } }
+
+  before do
+    allow_any_instance_of(ApplicationController).to receive(:verify_id_token).and_return(true)
+    allow_any_instance_of(ApplicationController).to receive(:firebase_user).and_return(firebase_user)
+  end
+
+  describe 'POST /api/v1/user' do
+    context 'when the request is valid' do
+      before do
+        allow_any_instance_of(User).to receive(:save).and_return(true)
+        post api_v1_user_path, params: user_params
+      end
+
+      it 'creates a user and returns status code 200' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'returns the user data' do
+        expect(response.parsed_body).to include('name' => 'new_user')
+      end
+    end
+
+    context 'when the request is invalid' do
+      before do
+        allow_any_instance_of(User).to receive(:save).and_return(false)
+        allow_any_instance_of(User).to receive_message_chain(:errors, :full_messages)
+          .and_return(['Invalid parameters'])
+        post api_v1_user_path, params: user_params
+      end
+
+      it 'returns status code 400' do
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it 'returns an error message' do
+        expect(response.parsed_body).to eq('errors' => ['Invalid parameters'])
+      end
+    end
   end
 end
