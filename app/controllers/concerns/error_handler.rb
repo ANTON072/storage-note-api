492d60a9bb871d7b3b2641df82b4ddc6aac7e8aa
Katsushi OUGI <ougi@to-r.net>
2023-07-07 00:16:26 +0900

    エラーハンドリング対応

--- app/controllers/concerns/error_handler.rb
+++ app/controllers/concerns/error_handler.rb
@@ -12,69 +12,60 @@ module ErrorHandler
   private
 
   def auth_invalid(error)
-    render_errors([{
-                    status: :unauthorized,
-                    title: 'Unauthorized',
-                    detail: error.message,
-                    path: request.path
-                  }])
+    errors = [{
+      status: :unauthorized,
+      title: 'Unauthorized',
+      detail: error.message,
+      path: request.path
+    }]
+    render json: { errors: }, status: errors.first[:status]
   end
 
   def token_expired(error)
-    render_errors([{
-                    status: :unauthorized,
-                    title: 'Token Expired',
-                    detail: error.message,
-                    path: request.path
-                  }])
+    errors = [{
+      status: :unauthorized,
+      title: 'Token Expired',
+      detail: error.message,
+      path: request.path
+    }]
+    render json: { errors: }, status: errors.first[:status]
   end
 
   def render_exception(error)
-    render_errors([{
-                    status: get_status(error),
-                    title: error.class.name.demodulize.underscore.humanize,
-                    detail: error.message,
-                    path: request.path
-                  }])
+    errors = [{
+      status: get_status(error),
+      title: error.class.name.demodulize.underscore.humanize,
+      detail: error.message,
+      path: request.path
+    }]
+    render json: { errors: }, status: errors.first[:status]
   end
 
   def get_status(error)
     case error
     when ActiveRecord::RecordNotFound
-      :not_found
+      404
     when ActionController::ParameterMissing
-      :bad_request
+      400
     else
-      :internal_server_error
+      500
     end
   end
 
   def record_invalid(error)
-    errors = error.record.errors.map do |field, message|
-      {
-        status: :unprocessable_entity,
-        title: 'Validation Error',
-        detail: "#{field.to_s.humanize} #{message}",
-        source: {
-          pointer: "/data/attributes/#{field}"
+    errors = error.record.errors.messages.map do |field, messages|
+      messages.map do |message|
+        {
+          status: 422,
+          title: 'Validation Error',
+          detail: "#{error.record.class.human_attribute_name(field)}#{message}",
+          source: {
+            pointer: field.to_s
+          }
         }
-      }
-    end
-    render_errors(errors)
+      end
+    end.flatten
+    render json: { errors: }, status: errors.first[:status]
   end
 
-  def render_errors(errors)
-    formatted_errors = errors.map do |error|
-      {
-        status: error[:status].to_s,
-        title: error[:title],
-        detail: error[:detail],
-        source: {
-          pointer: error[:path]
-        }
-      }
-    end
-
-    render json: { errors: formatted_errors }, status: errors.first[:status]
-  end
 end
