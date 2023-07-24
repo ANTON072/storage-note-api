667b58219490a29c5e87e626fcffa2513b29af85
Katsushi OUGI <ougi@to-r.net>
2023-07-24 14:10:47 +0900

    errorsのロード位置を変更してみる

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,8 @@ module StorageNote
     config.load_defaults 7.0
     config.api_only = true
     config.time_zone = 'Tokyo'
+    config.autoload_paths += %W[#{config.root}/app/errors]
     config.i18n.default_locale = :ja
     config.firebase = config_for(:firebase)
-    config.autoload_paths += %W[#{config.root}/app/errors]
   end
 end
